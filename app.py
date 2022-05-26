d85669bed4e489032846bb9bda6ea6143b5c0cc1
sentenzo <kassiyskley@rambler.ru>
2022-05-26 20:23:02 +0300

    init

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, render_template, request, flash
+
+app = Flask(__name__)
+
+
+@app.route("/hello")
+def index():
+    flash("what's your name?")
+    return render_template("index.html")
+
+
+@app.route("/greet", methods=["POST", "GET"])
+def greet():
+    flash("Hi " + str(request.form["name_input"]) + "!")
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(24)
+    app.run()
