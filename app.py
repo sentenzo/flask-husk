997fe8bcc2a06fbfb89193b6b4d14347920d3384
sentenzo <kassiyskley@rambler.ru>
2022-05-28 15:27:47 +0300

    network latency simulation

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 import os
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request
+from time import sleep
+from random import random
+
+DBG = True
+
+
+def dbg_rand_sleep(a=0.1, b=0.8):
+    if DBG:
+        d = b - a
+        if d > 0:
+            sleep(random()*d + a)
+        return
+
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -14,11 +27,13 @@ def index():
 
 @app.route("/ajax/get_counter", methods=["GET"])
 def ajax_get_counter():
-    return {"counter": counter}  # f"{{counter: {counter}}}"
+    dbg_rand_sleep()
+    return {"counter": counter}
 
 
 @app.route("/ajax/inc_counter", methods=["POST"])
 def ajax_inc_counter():
+    dbg_rand_sleep()
     global counter
     diff = 1
     if "diff" in request.form:
